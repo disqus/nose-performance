71241579d678185eb315ba2658f1c7eb9ec75603
David Cramer <dcramer@gmail.com>
2012-08-28 14:11:46 -0700

    Add basic cache test example

--- example/django/tests.py
+++ example/django/tests.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from django.core.cache import cache
 from django.contrib.auth.models import User
 from noseperf.testcases import DjangoPerformanceTest
 
@@ -8,3 +9,8 @@ class DjangoSampleTest(DjangoPerformanceTest):
     def test_create_a_bunch_of_users(self):
         for n in xrange(2 ** 8):
             User.objects.create(username='test-%d' % n, email='test-%d@example.com' % n)
+
+    def test_use_the_cache(self):
+        for n in xrange(2 ** 6):
+            cache.set('test-%s' % (n - 1), n)
+            cache.set('test-%s-%s' % (n, n - 1), cache.get('test-%s' % (n - 1)))
