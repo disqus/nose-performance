221e0f6ec0d692df862f1a19bed056743664f027
David Cramer <dcramer@gmail.com>
2012-08-28 18:51:36 -0700

    Speed up  tests and add one more composite test

--- example/django/tests.py
+++ example/django/tests.py
@@ -7,10 +7,15 @@ from noseperf.testcases import DjangoPerformanceTest
 
 class DjangoSampleTest(DjangoPerformanceTest):
     def test_create_a_bunch_of_users(self):
-        for n in xrange(2 ** 8):
+        for n in xrange(2 ** 5):
             User.objects.create(username='test-%d' % n, email='test-%d@example.com' % n)
 
     def test_use_the_cache(self):
-        for n in xrange(2 ** 6):
+        for n in xrange(2 ** 5):
             cache.set('test-%s' % (n - 1), n)
             cache.set('test-%s-%s' % (n, n - 1), cache.get('test-%s' % (n - 1)))
+
+    def test_the_world(self):
+        for n in xrange(2 ** 5):
+            cache.set('test-%s' % n, n)
+            User.objects.create(username='test-%d' % n, email='test-%d@example.com' % n)
--- example/redis/tests.py
+++ example/redis/tests.py
@@ -15,11 +15,11 @@ class RedisSampleTest(PerformanceTest):
         self.client.delete(self.key)
 
     def test_setting_lots_of_hashes(self):
-        for n in xrange(2 ** 8):
+        for n in xrange(2 ** 5):
             self.client.hset(self.key, str(n), time.time())
 
     def test_pipeline_hashes(self):
         with self.client.pipeline() as conn:
-            for n in xrange(2 ** 8):
+            for n in xrange(2 ** 5):
                 conn.hset(self.key, str(n), time.time())
             conn.execute()
