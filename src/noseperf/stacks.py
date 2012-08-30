16e5b88efcdae9cb18465ac72ad2838cfa49170d
David Cramer <dcramer@gmail.com>
2012-08-29 21:43:33 -0700

    Hide get_stack_info frame

--- src/noseperf/stacks.py
+++ src/noseperf/stacks.py
@@ -113,6 +113,8 @@ def get_stack_info(frames):
     _Frame class do not contain the nescesary data to lookup all
     of the information we want.
     """
+    __traceback_hide__ = True  # NOQA
+
     results = []
     for frame_info in frames:
         # Old, terrible API
